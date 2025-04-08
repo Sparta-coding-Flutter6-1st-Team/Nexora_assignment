5454a97772f12ac99b75c479de0cb5482a293d2d
SeongMin Jaden Lee <101249717+SeongminJaden@users.noreply.github.com>
2025-04-08 10:29:27 +0900

    feat : Merge Page - intro page(jeongmin)

--- _4weeks/lib/main.dart
+++ _4weeks/lib/main.dart
@@ -3,6 +3,7 @@ import 'pages/product_detail_page.dart';
 import 'pages/cart_list_page.dart';
 import 'pages/payment_info_page.dart';
 import 'pages/product_list_page.dart';
+import 'pages/intro_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       initialRoute: '/',
       routes: {
-        '/': (context) => const ProductListPage(),
+        '/': (context) => const DongdongIntroPage(),
+        '/intro': (context) => const DongdongIntroPage(),
         '/cart': (context) => const CartListPage(),
         '/payment': (context) => const PaymentInfoPage(),
       },
