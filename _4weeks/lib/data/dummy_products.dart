a3ee5637a147caeeaf7c4fd3dad833f84b76b813
SeongMin Jaden Lee <101249717+SeongminJaden@users.noreply.github.com>
2025-04-08 11:42:41 +0900

    fix : conflicts files

---
+++ _4weeks/lib/data/dummy_products.dart
@@ -0,0 +1,49 @@
+import '../models/product.dart';
+
+final List<Product> dummyProducts = [
+  Product(
+    name: '귀여운 티셔츠',
+    imageUrl: 'https://via.placeholder.com/150',
+    price: 12000,
+  ),
+  Product(
+    name: '여름 반바지',
+    imageUrl: 'https://via.placeholder.com/150',
+    price: 18000,
+  ),
+  Product(
+    name: '플러터 스티커',
+    imageUrl: 'https://via.placeholder.com/150',
+    price: 3000,
+  ),
+  Product(
+    name: '귀여운 티셔츠',
+    imageUrl: 'https://via.placeholder.com/150',
+    price: 12000,
+  ),
+  Product(
+    name: '여름 반바지',
+    imageUrl: 'https://via.placeholder.com/150',
+    price: 18000,
+  ),
+  Product(
+    name: '플러터 스티커',
+    imageUrl: 'https://via.placeholder.com/150',
+    price: 3000,
+  ),
+  Product(
+    name: '귀여운 티셔츠',
+    imageUrl: 'https://via.placeholder.com/150',
+    price: 12000,
+  ),
+  Product(
+    name: '여름 반바지',
+    imageUrl: 'https://via.placeholder.com/150',
+    price: 18000,
+  ),
+  Product(
+    name: '플러터 스티커',
+    imageUrl: 'https://via.placeholder.com/150',
+    price: 3000,
+  ),
+];
